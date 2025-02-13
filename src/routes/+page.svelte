ce4c82a3e309587605538753f1178abdeb0ce078
BillyBoy16 <lennyloy07@gmail.com>
2025-02-13 10:58:53 +0100

    ornigramme_v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,32 @@
-<h1>Welcome to SvelteKit</h1>
-
 <h2>Présentation de l'association</h2>
 <p>Ingénieur-e au féminin est une association de Polytech Annecy-Chambéry qui œuvre pour la promotion de l’égalité femmes-hommes 
     dans les études scientifiques. Son objectif est d’encourager les jeunes filles à s’orienter vers les carrières d’ingénierie 
     et de sensibiliser l’ensemble des étudiants aux enjeux de mixité dans ces domaines. Le bureau de l’association est réparti 
     sur les deux sites de Polytech Annecy-Chambéry, permettant ainsi une présence active sur l’ensemble du campus.</p>
 
+
+<h2>Organigramme</h2>
+<div class="chartH1Container">
+    <span class="chartH1">Bureau Restreint</span>
+  </div>
+  <div class="chartGrid">
+    <div class="chartModule">
+      <span class="chartName">Emma Rechon-Reguet</span>
+      <hr class="chartLine">
+      <span class="chartTitle">Présidente</span>
+      <img src="/images_ornigramme/presidente.png" alt="photo_presidente" />
+    </div>
+  </div>
+  <div class="chartH1Container">
+    <span class="chartH1">Bureau élargie</span>
+  </div>
+  <div class="chartGrid">
+    <div class="chartModule">
+      <span class="chartName">Prénom nom</span>
+      <hr class="chartLine">
+      <span class="chartTitle">Vice-présidente</span>
+    </div>
+  </div>
 <h2>Nos actions et événements</h2>
 <p>Tout au long de l’année, Ingénieur-e au féminin mène diverses actions pour promouvoir la place des femmes dans 
     l’ingénierie. L’association intervient dans les collèges et lycées pour déconstruire les stéréotypes et 
